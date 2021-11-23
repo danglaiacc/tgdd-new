bbf701c71612057c2bcba48c2bdd9366907cbc74
lai laptop <danglaiacc@gmail.com>
2021-11-23 17:05:02 +0700

    fix (pipeline): done add comment

--- pipeline.py
+++ pipeline.py
@@ -3,7 +3,11 @@
 spark-submit --conf spark.ui.port=5051 pipeline.py
 '''
 from pyspark.sql import SparkSession, functions as f
-from pyspark.sql.types import IntegerType, StringType
+from pyspark.sql.types import DateType, IntegerType, StringType, TimestampType
+import random
+from datetime import datetime
+
+
 spark = SparkSession.builder\
     .appName('ETL Pipeline')\
     .master('local[2]')\
@@ -11,11 +15,14 @@ spark = SparkSession.builder\
 
 phone_path = './getphone/phones.csv'
 color_path = './getphone/colors.csv'
+comment_path = './getphone/comments.csv'
+
 driver = 'org.postgresql.Driver'
 url = "jdbc:postgresql://localhost/tgdd?user=postgres&password=12345"
 
 ''' done add phone and series '''
 # EXTRACT
+'''
 phone_df = spark.read\
     .option('header', 'true')\
     .option('inferSchema', 'true')\
@@ -38,21 +45,6 @@ series_df = phone_df.select('manu_name', 'series_name')\
     .withColumn('manu_id', get_manu_id_udf(f.col('manu_name')))\
     .drop('manu_name')
 
-'''
-series_df_origin = spark.read\
-    .format('jdbc')\
-    .options(
-        url=url,
-        driver=driver,
-        dbtable='series'
-    ).load()
-# load to series table
-write_df = series_df.join(
-    series_df_origin,
-    (series_df.name == series_df_origin.name) &
-    (series_df.manu_id == series_df_origin.manu_id),
-    how='left_anti')
-'''
 series_df\
     .write\
     .format('jdbc')\
@@ -132,5 +124,69 @@ color_df.write\
     )\
     .mode('append')\
     .save()
+'''
+
+comment_df = spark.read\
+    .option('header', 'true')\
+    .option('inferSchema', 'true')\
+    .option("quote", "\"")\
+    .option("escape", "\"")\
+    .format('csv')\
+    .load(comment_path)
+
+'''
+customer_df = comment_df.selectExpr(
+    'customer_id as id',
+    'customer_fullname as fullname')
 
 
+customer_df.write\
+    .format('jdbc')\
+    .options(
+        url=url,
+        driver=driver,
+        dbtable='customer'
+    )\
+    .mode('append')\
+    .save()
+
+# comment_imgs,content,date_buy,product_id,rate_star,time_up,customer_id,customer_fullname
+'''
+
+
+def random_date(time_up):
+    if time_up != None:
+        return time_up+' 0:0:0'
+    first_timestamp = 1546324200
+    second_timestamp = 1642197000
+    random_timestamp = random.randint(first_timestamp, second_timestamp)
+    return datetime\
+        .fromtimestamp(random_timestamp)\
+        .strftime('%d/%m/%Y %H:%M:%S')
+
+
+timeup_udf = f.udf(
+    lambda time_up: random_date(time_up),
+    StringType()
+)
+
+comment_df.select(
+    'customer_id',
+    'product_id',
+    'time_up',
+    'content',
+    'rate_star'
+)\
+    .withColumn(
+    'time_up',
+    f.to_timestamp(
+        timeup_udf(f.col('time_up')),
+        'd/M/y H:m:s'))\
+    .write\
+    .format('jdbc')\
+    .options(
+        url=url,
+        driver=driver,
+        dbtable='comment')\
+    .mode('append')\
+    .save()
